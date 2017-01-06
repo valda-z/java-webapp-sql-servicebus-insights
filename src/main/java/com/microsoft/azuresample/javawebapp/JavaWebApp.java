a796af3cd79add72df7357d0161c6e15b357a135
valda-z <valdemar.zavadsky@microsoft.com>
2017-01-06 20:28:47 +0100

    Solution with App insight on server side

--- src/main/java/com/microsoft/azuresample/javawebapp/JavaWebApp.java
+++ src/main/java/com/microsoft/azuresample/javawebapp/JavaWebApp.java
@@ -1,14 +1,39 @@
 package com.microsoft.azuresample.javawebapp;
 
+import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class JavaWebApp {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class JavaWebApp  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(JavaWebApp.class, args);
         System.out.println("My Spring Boot app started ...");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<JavaWebApp> applicationClass = JavaWebApp.class;
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        final WebRequestTrackingFilter webRequestTrackingFilter = new WebRequestTrackingFilter();
+        filterRegistrationBean.setFilter(webRequestTrackingFilter);
+        return filterRegistrationBean;
     }
 }
